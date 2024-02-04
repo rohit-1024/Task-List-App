93f8e30ff63b2ac613debff7c7fd99d5d8f2378a
Rohit_V_Raut <142472226+rohit-1024@users.noreply.github.com>
2024-02-04 18:28:26 +0530

    TaskListApp.java

--- TaskListApp.java
+++ TaskListApp.java
@@ -1,3 +1,7 @@
+Develop a basic Java application that allows users to create a task list.
+Users should be able to add, remove, and list tasks.
+The program should have a simple text-based user interface.
+    
 import java.util.ArrayList;
 import java.util.Scanner;
 
